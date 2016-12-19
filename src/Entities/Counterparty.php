6f0ee112717823592ed00be3118780187b34c5e0
Tooyz <Tooy_m@mail.ru>
2016-12-20 00:23:55 +0300

    make counterparty creatable

--- src/Entities/Counterparty.php
+++ src/Entities/Counterparty.php
@@ -2,7 +2,21 @@
 
 namespace MoySklad\Entities;
 
-class Counterparty extends AbstractEntity{
+use MoySklad\Components\MassRequest;
+use MoySklad\Interfaces\ICreatable;
+
+class Counterparty extends AbstractEntity implements ICreatable {
     public static
         $entityName = 'counterparty';
+
+    public function setCreate()
+    {
+
+    }
+
+    public function doCreate()
+    {
+        $mr = new MassRequest($this->getSkladInstance(), $this);
+        return $mr->create()[0];
+    }
 }
