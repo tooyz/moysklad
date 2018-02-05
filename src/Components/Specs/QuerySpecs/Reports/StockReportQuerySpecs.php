9bd3360f5a1e9f1e76f41e8450b242fb1939a465
Ashe <ashe.29@ya.ru>
2018-02-05 23:27:37 +0300

    Added all URI parameters for StockReportQuerySpecs

--- src/Components/Specs/QuerySpecs/Reports/StockReportQuerySpecs.php
+++ src/Components/Specs/QuerySpecs/Reports/StockReportQuerySpecs.php
@@ -11,6 +11,17 @@ class StockReportQuerySpecs extends QuerySpecs {
     public function getDefaults()
     {
         $res = parent::getDefaults();
+        $res['store.id'] = null;
+        $res['product.id'] = null;
+        $res['consignment.id'] = null;
+        $res['variant.id'] = null;
+        $res['productFolder.id'] = null;
+        $res['search'] = null;
+        $res['stockMode'] = null;
+        $res['groupBy'] = null;
+        $res['moment'] = null;
+        $res['characteristics'] = null;
+        $res['includeRelated'] = null;
         $res['operation.id'] = null;
         return $res;
     }
