f2f9b0b8786a33e3c139a35da9a188b1baba58b3
Artem Yurov <artem@yurov.org>
2020-02-11 00:19:16 +0300

    Update Attribute.php

--- src/Entities/Misc/Attribute.php
+++ src/Entities/Misc/Attribute.php
@@ -6,4 +6,13 @@ use MoySklad\Entities\AbstractEntity;
 
 class Attribute extends AbstractEntity  {
     public static $entityName = 'attributemetadata';
+
+    public function jsonSerialize()
+    {
+        $res = $this->fields->getInternal();
+        $res->relations = $this->relations;
+        $res->meta = $this->meta;
+        return $res;
+    }
+
 }
