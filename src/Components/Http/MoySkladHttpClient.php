41d7dde564d3da223453e248b0db6c36a3b0eb02
EugeneGpil <ev@freshcube.ru>
2020-03-12 14:17:49 +0300

    endpoint fix

--- src/Components/Http/MoySkladHttpClient.php
+++ src/Components/Http/MoySkladHttpClient.php
@@ -30,7 +30,7 @@ class MoySkladHttpClient{
         $this->login = $login;
         $this->password = $password;
         $this->posToken = $posToken;
-        $this->endpoin = "https://" . $subdomain . ".moysklad.ru/api/remap/1.1/";
+        $this->endpoint = "https://" . $subdomain . ".moysklad.ru/api/remap/1.1/";
     }
 
     public function setPosToken($posToken){
