21b4e338d04fe9357356d0755b95b598362b3339
Max Podkosov <mack_91@mail.ru>
2020-06-13 19:01:38 +0300

    Добавление сущности перемещения

--- src/Entities/Documents/Movements/Move.php
+++ src/Entities/Documents/Movements/Move.php
@@ -2,6 +2,12 @@
 
 namespace MoySklad\Entities\Documents\Movements;
 
+use MoySklad\Entities\Organization;
+
 class Move extends AbstractMovement {
     public static $entityName = 'move';
+    public static function getFieldsRequiredForCreation()
+    {
+        return [Organization::$entityName, 'targetStore', 'sourceStore'];
+    }
 }
