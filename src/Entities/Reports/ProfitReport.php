ff254598b9e2c1b7c9c4b496784c0b40741f258f
A.V <ksm@kolo.ws>
2020-10-24 12:35:27 +0300

    Profit report entity. (Продажи -> Прибыльность)

---
+++ src/Entities/Reports/ProfitReport.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace MoySklad\Entities\Reports;
+
+use MoySklad\Components\Specs\QuerySpecs\Reports\ProfitReportQuerySpecs;
+use MoySklad\Entities\Documents\AbstractDocument;
+use MoySklad\MoySklad;
+
+class ProfitReport extends AbstractReport  {
+    public static $reportName = 'profit';
+
+    public static function byProduct(MoySklad $sklad, ProfitReportQuerySpecs $specs = null){
+        return static::queryWithParam($sklad, 'byproduct', $specs);
+    }
+    public static function byEmployee(MoySklad $sklad, ProfitReportQuerySpecs $specs = null){
+        return static::queryWithParam($sklad, 'byemployee', $specs);
+    }
+
+}
