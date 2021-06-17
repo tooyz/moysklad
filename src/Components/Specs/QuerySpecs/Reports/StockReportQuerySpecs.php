f00a75a85b55a8df8b3dd3a605e5bb3c8d1dc847
Artem Yurov <artem@yurov.org>
2021-06-17 18:44:45 +0500

    Update StockReportQuerySpecs.php

--- src/Components/Specs/QuerySpecs/Reports/StockReportQuerySpecs.php
+++ src/Components/Specs/QuerySpecs/Reports/StockReportQuerySpecs.php
@@ -18,11 +18,13 @@ class StockReportQuerySpecs extends QuerySpecs {
         $res['productFolder.id'] = null;
         $res['search'] = null;
         $res['stockMode'] = null;
+        $res['quantityMode'] = null; // temp hack
         $res['groupBy'] = null;
         $res['moment'] = null;
         $res['characteristics'] = null;
         $res['includeRelated'] = null;
         $res['operation.id'] = null;
+        $res['filter'] = null; // temp hack
         return $res;
     }
 }
